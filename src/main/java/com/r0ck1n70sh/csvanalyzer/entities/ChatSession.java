5ef4bf0895217cd0cc6c758e77996645767eab3a
r0ck1n70sh <r0ck1n70sh@gmail.com>
2023-10-24 21:37:55 +0530

    added -- ChatEntityMessage, csv, status -- ChatSession

--- src/main/java/com/r0ck1n70sh/csvanalyzer/entities/ChatSession.java
+++ src/main/java/com/r0ck1n70sh/csvanalyzer/entities/ChatSession.java
@@ -1,14 +1,21 @@
 package com.r0ck1n70sh.csvanalyzer.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.r0ck1n70sh.csvanalyzer.enums.ChatStatus;
 import com.r0ck1n70sh.csvanalyzer.utils.StringUtils;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 @Table(name = "CHAT_SESSION")
 @Getter
+@Setter
 public class ChatSession {
     public static final int CHAT_SESSION_ID_LEN = 22;
 
@@ -19,10 +26,23 @@ public class ChatSession {
     private final String chatSessionId;
 
     @ManyToOne
-    @JoinColumn(name="user_session_id")
+    @JoinColumn(name = "user_session_id")
+    @JsonIgnore
     private UserSession userSession;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "chatSession")
+    @ElementCollection
+    private List<ChatMessageEntity> messages;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "chatSession")
+    private RawCsvMeta csv;
+
+    @Enumerated
+    private ChatStatus status;
 
     public ChatSession() {
         this.chatSessionId = StringUtils.generateRandomAlphaNumericString(CHAT_SESSION_ID_LEN);
+        this.status = ChatStatus.INITIALIZED;
+        this.messages = new ArrayList<>();
     }
 }
