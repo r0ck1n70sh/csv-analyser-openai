8b8a6309daa192735cccb83bfcb9a347d9f642bc
r0ck1n70sh <r0ck1n70sh@gmail.com>
2023-10-26 10:39:03 +0530

    ChatMessageEntity.java -> added -- fields "functionCallName" & "functionCallArgs", methods "toMessage"

--- src/main/java/com/r0ck1n70sh/csvanalyzer/entities/ChatMessageEntity.java
+++ src/main/java/com/r0ck1n70sh/csvanalyzer/entities/ChatMessageEntity.java
@@ -2,12 +2,15 @@ package com.r0ck1n70sh.csvanalyzer.entities;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.r0ck1n70sh.csvanalyzer.enums.ChatMessageEntityRole;
+import com.r0ck1n70sh.csvanalyzer.utils.JsonUtils;
+import com.theokanning.openai.completion.chat.ChatFunctionCall;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.Map;
 
 
 @Table(name = "CHAT_MESSAGE_ENTITY")
@@ -20,24 +23,60 @@ public class ChatMessageEntity {
     private long id;
 
     @Enumerated
-    private final ChatMessageEntityRole role;
+    private ChatMessageEntityRole role;
 
-    private final String name;
+    private String name;
 
-    private final String content;
+    @Column(columnDefinition = "TEXT")
+    private String content;
 
     @ManyToOne
     @JoinColumn(name = "chat_session_id")
     @JsonIgnore
     private ChatSession chatSession;
 
+    private String functionCallName;
+
+    @ElementCollection
+    @MapKeyColumn(name = "name")
+    @Column(name = "value")
+    @CollectionTable(name = "function_call_args", joinColumns = @JoinColumn(name = "function_call_args_id"))
+    private Map<String, String> functionCallArgs;
+
     private final long timestamp;
 
     public ChatMessageEntity(@NonNull ChatMessage chatMessage) {
+        this();
+
         this.role = ChatMessageEntityRole.fromRole(chatMessage.getRole());
         this.name = chatMessage.getName();
         this.content = chatMessage.getContent();
+        this.functionCallName = chatMessage.getFunctionCall() == null
+                ? null
+                : chatMessage.getFunctionCall().getName();
+
+        this.functionCallArgs = chatMessage.getFunctionCall() == null
+                ? null
+                : JsonUtils.jsonNodeAsMap(chatMessage.getFunctionCall().getArguments());
+    }
+
+    public ChatMessageEntity() {
         this.timestamp = System.currentTimeMillis();
+    }
+
+    public ChatMessage toMessage() {
+        ChatFunctionCall chatFunction = null;
+
+        try {
+            chatFunction = functionCallName == null
+                    ? null
+                    : new ChatFunctionCall(functionCallName, JsonUtils.mapToJsonNode(functionCallArgs));
+        } catch (JsonProcessingException e) {
+
+        }
+
+
+        return new ChatMessage(role.name, content, name, chatFunction);
     }
 
     @Override
