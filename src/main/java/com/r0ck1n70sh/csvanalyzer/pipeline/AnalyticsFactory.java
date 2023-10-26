09fa9dfce39f8d2d672f5819124fff7c6a607c4e
r0ck1n70sh <r0ck1n70sh@gmail.com>
2023-10-26 10:43:07 +0530

    implemented -- AnalyticsFactory

--- src/main/java/com/r0ck1n70sh/csvanalyzer/pipeline/AnalyticsFactory.java
+++ src/main/java/com/r0ck1n70sh/csvanalyzer/pipeline/AnalyticsFactory.java
@@ -1,10 +1,14 @@
 package com.r0ck1n70sh.csvanalyzer.pipeline;
 
 import com.r0ck1n70sh.csvanalyzer.entities.ChatSession;
+import lombok.Setter;
 
+@Setter
 public class AnalyticsFactory implements ComponentFactory<AnalyticsComponent> {
+    private String userPrompt;
+
     @Override
     public AnalyticsComponent create(ChatSession chatSession) {
-        return null;
+        return new AnalyticsComponent(userPrompt, chatSession.getMessages(), chatSession.getCsv());
     }
 }
